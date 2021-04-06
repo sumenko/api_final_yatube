4930e0d717b13bd46be633b62ad8f3c8b096521a
Vladimir Sumenko <3760097@gmail.com>
2021-04-06 20:24:13 +0300

    add jwt token support

--- yatube_api/urls.py
+++ yatube_api/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import TemplateView
+from django.urls import include, path, re_path
+
+
+from django.views.generic import TemplateView, RedirectView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, 
+    TokenRefreshView
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
     path('api/v1/', include('api.urls')),
     path(
         'redoc/',
